ad543720522d1e348cb0531dbd525b8b44fa3b9a
kunallad54 <kunallad5473@gmail.com>
2021-06-28 10:34:45 +0530

    Validated Last Name using Regex

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -1,6 +1,6 @@
 /***************************************************************************************************
- *
- * Purpose : As a User need to enter a valid First Name
+ * UC2
+ * Purpose : As a User need to enter a valid Last Name
  *          - First name starts with Cap and has
  *            minimum 3 characters
  *
@@ -16,18 +16,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
+    static UserRegistrationProblem usr = new UserRegistrationProblem();
+
     public static void main(String[] args) {
 
-        UserRegistrationProblem usr = new UserRegistrationProblem();
-        Scanner scanner = new Scanner(System.in);
+        usr.userConsole();
 
-        System.out.println("Enter the Name :");
-        String firstName = scanner.next();
-
-        if (usr.checkFirstName(firstName))
-            System.out.println("Valid First Name");
-        else
-            System.out.println("Invalid First Name");
     }
 
     /**
@@ -50,6 +44,64 @@ public class UserRegistrationProblem {
         Matcher m = p.matcher(firstName);
 
         return m.matches();
+
+    }
+
+    /**
+     * Purpose : Check whether last name entered is valid or not it should start with cap
+     *           and should have minimum 3 chars
+     *
+     * @param lastName input from user
+     * @return true if valid last name else false
+     */
+    public boolean checkLastName(String lastName) {
+
+        //first letter should be cap and should have min 3 letters
+        String regex = "^[A-Z][a-z]{2,}";
+        Pattern p = Pattern.compile(regex);
+
+        if (lastName == null) {
+            return false;
+        }
+
+        Matcher m = p.matcher(lastName);
+
+        return m.matches();
+
+    }
+
+    /**
+     * Purpose : User console to take input from user
+     */
+    public void userConsole(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the First Name :");
+        String firstName = scanner.next();
+
+        System.out.println("Enter the Last Name :");
+        String lastName = scanner.next();
+
+        usr.validateDetails(firstName,lastName);
+    }
+
+    /**
+     * Purpose : It validate user details
+     *
+     * @param firstName input from user
+     * @param lastName input from user
+     */
+    public void validateDetails(String firstName, String lastName){
+
+        if (usr.checkFirstName(firstName))
+            System.out.println("Valid First Name");
+        else
+            System.out.println("Invalid First Name");
+
+        if(usr.checkLastName(lastName))
+            System.out.println("Valid Last Name");
+        else
+            System.out.println("Invalid Last Name");
 
     }
 }
